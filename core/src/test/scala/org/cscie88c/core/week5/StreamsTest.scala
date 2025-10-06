cd7e0effd85f10a7be2a6df84a28daaa06f7c003
venkatesa <vtadipatri@gmail.com>
2025-10-06 18:23:10 -0400

    Get week 5 main code

---
+++ core/src/test/scala/org/cscie88c/core/week5/StreamsTest.scala
@@ -0,0 +1,9 @@
+package org.cscie88c.core.week5
+
+import org.cscie88c.core.testutils.{ StandardTest }
+import Streams.Dog
+
+class StreamsTest extends StandardTest {
+
+  // Bonus problem unit tests
+}
