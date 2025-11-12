6f125809add716e2f1a522f0659b10d3751880de
venkatesa <vtadipatri@gmail.com>
2025-11-11 22:19:42 -0500

    Add debugging

--- beam/src/main/scala/org/cscie88c/beam/BeamJob.scala
+++ beam/src/main/scala/org/cscie88c/beam/BeamJob.scala
@@ -7,9 +7,12 @@ import com.spotify.scio._
 // sbt beam/run
 object BeamJob {
   def main(args: Array[String]): Unit = {
+    println("Starting beam job...")
     val (sc: ScioContext, jobArgs: Args) = ContextAndArgs(args)
     implicit val scImplicit: ScioContext = sc
     val inputFile = jobArgs.getOrElse("input-file", "../data/alice_in_wonderland.txt")
+    val checkFile = new java.io.File(inputFile)
+    println(s"${checkFile.getAbsolutePath} exists = ${checkFile.exists}")
     val outputFile = jobArgs.getOrElse("output-file", "./target/alice_word_count")
     // Define the pipeline
 
